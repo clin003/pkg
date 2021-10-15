a5d99351953b1e5550ee9ed3173c3146d6bc3977
白菜林 <clin003@vip.qq.com>
2021-10-15 20:38:31 +0800

    add SendResponseEx

--- handler/handler.go
+++ handler/handler.go
@@ -13,7 +13,17 @@ type Response struct {
 	Data    interface{} `json:"data"`
 }
 
-// Json
+// 返回 Json
+// {
+//   "code": 0,
+//   "message": "",
+//   "data": {
+//     ...其他字段
+//   }
+// }
+//     code: 返回 0，表示当前接口正确返回，否则按错误请求处理；
+//     message: 返回接口处理信息，主要用于表单提交或请求失败时的 toast 显示；
+//     data: 必须返回一个结构的对象。
 func SendResponse(c *gin.Context, err error, data interface{}) {
 	code, message := errno.DecodeErr(err)
 
@@ -22,6 +32,34 @@ func SendResponse(c *gin.Context, err error, data interface{}) {
 		Code:    code,
 		Message: message,
 		Data:    data,
+	})
+}
+
+type ResponseEx struct {
+	Status int         `json:"status"`
+	Msg    string      `json:"msg"`
+	Data   interface{} `json:"data"`
+}
+
+// 返回 Json
+// {
+//   "status": 0,
+//   "msg": "",
+//   "data": {
+//     ...其他字段
+//   }
+// }
+//     status: 返回 0，表示当前接口正确返回，否则按错误请求处理；
+//     msg: 返回接口处理信息，主要用于表单提交或请求失败时的 toast 显示；
+//     data: 必须返回一个结构的对象。
+func SendResponseEx(c *gin.Context, err error, data interface{}) {
+	code, message := errno.DecodeErr(err)
+
+	//always return http.StatusOK
+	c.JSON(http.StatusOK, ResponseEx{
+		Status: code,
+		Msg:    message,
+		Data:   data,
 	})
 }
 
