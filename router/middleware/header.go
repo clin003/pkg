000b2b07a7ef79252fb8ea7d510beb90bfd84d67
白菜林 <clin003@vip.qq.com>
2021-10-26 10:44:56 +0800

    add header middleware allow origin

--- router/middleware/header.go
+++ router/middleware/header.go
@@ -23,7 +23,15 @@ func Options(c *gin.Context) {
 	if c.Request.Method != "OPTIONS" {
 		c.Next()
 	} else {
-		c.Header("Access-Control-Allow-Origin", "*")
+		withCredentials := "true"
+		origin := c.Request.Header.Get("Origin")
+		if len(origin) == 0 {
+			origin = "*"
+			withCredentials = "false"
+		}
+		c.Header("Access-Control-Allow-Credentials", withCredentials)
+		c.Header("Access-Control-Allow-Origin", origin)
+		// c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept, x-action-addr")
 		c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
@@ -35,9 +43,23 @@ func Options(c *gin.Context) {
 //secure is a middleware function that appends security
 //and resource access headers.
 func Secure(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
+	withCredentials := "true"
+	origin := c.Request.Header.Get("Origin")
+	if len(origin) == 0 {
+		origin = "*"
+		withCredentials = "false"
+	}
+	// fmt.Println(origin)
+	// for k, v := range c.Request.Header {
+	// 	fmt.Println(k, v)
+	// }
+	// fmt.Println(c.Request.URL)
+	c.Header("Access-Control-Allow-Credentials", withCredentials)
+	c.Header("Access-Control-Allow-Origin", origin)
+	// c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("X-Frame-Options", "DENY")
 	c.Header("X-Content-Type-Options", "nosniff")
+	// https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/X-XSS-Protection
 	c.Header("X-XSS-Protection", "1; mode=block")
 	if c.Request.TLS != nil {
 		c.Header("Strict-Transport-Security", "max-age=31536000")
